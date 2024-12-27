3f685588367a6e0eda858b003146f7871f3d5359
BesaMulajKrasniqi <besasmulaj@gmail.com>
2024-12-27 14:23:39 +0100

    Update input.c

--- project3/src/input.c
+++ project3/src/input.c
@@ -29,7 +29,7 @@ void read_molecule(FILE* input_file, size_t Natoms, double** coord, double* mass
 
     for (size_t i = 0; i < Natoms; i++) {
         if (fscanf(input_file, "%lf %lf %lf %lf", &coord[i][0], &coord[i][1], &coord[i][2], &mass[i]) !=4) {
-            fprint(stderr, "Error: Failed to read dtaa.\n", i);
+            fprint(stderr, "Error: Failed to read data for atom %zu.\n", i);
             exit(EXIT_FAILURE);
         }
     }
