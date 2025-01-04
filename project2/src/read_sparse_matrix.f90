59dc68c92c02dc501574563686280e5db8f0ccb6
BesaMulajKrasniqi <besasmulaj@gmail.com>
2025-01-04 18:09:30 +0100

    Update read_sparse_matrix.f90
    Added some lines of codes for metadata handling

--- project2/src/read_sparse_matrix.f90
+++ project2/src/read_sparse_matrix.f90
@@ -1,33 +1,40 @@
 program read_sparse_matrix
     implicit none
     integer, parameter :: max_entries = 10000  ! Adjust based on matrix size
-    integer :: i, n, row, col
+    integer, parameter :: max_metadata_lines = 5  ! Allow up to 5 lines of metadata
+    integer :: i, n, metadata_lines, row, col
     real(8) :: value
-    integer, allocatable :: rows(:), cols(:)
-    real(8), allocatable :: values(:)
+    integer, dimension(max_entries) :: rows, cols
+    real(8), dimension(max_entries) :: values
     character(len=256) :: filename
+    character(len=256) :: current_line
 
     ! Input file name
     print *, "Enter the file name: "
     read(*,*) filename
 
     open(unit=10, file=filename, status='old')
-    n = 0
-    do
-        read(10, *, iostat=i)
-        if (i /= 0) exit
-        n = n + 1
+    metadata_lines = 0
+    do i =1, max_metadata_lines
+        read(10, '(A)', iostat=line_check) current_line
+        if (line_check /= 0) exit
+        if (verify(adjust(current_line) '123456789') /= 0) then
+            metadata_lines = metadata_lines + 1
+        else
+            rewind(10)
+            exit
+        end if
     end do
-    rewind(10)
-    
-    allocate(rows(n), cols(n), values(n))
 
     ! Read the data
-    do i = 1, n
-        read(10, *) row, col, value
-        rows(i) = row
-        cols(i) = col
-        values(i) = value
+    n = 0
+    do
+        read(10, *, iostat=line_check) row, col, value
+        if (line_check /= 0) exit
+        n = n + 1
+        rows(n) = row
+        cols(n) = col
+        values(n) = value
     end do
     close(10)
 
