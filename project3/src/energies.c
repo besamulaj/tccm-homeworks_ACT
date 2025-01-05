9739b5e1da781147428a739f3b314364a173a1e8
bilgecetinn <bilgecetin306@gmail.com>
2025-01-05 16:20:26 +0100

    Update energies.c

--- project3/src/energies.c
+++ project3/src/energies.c
@@ -1,5 +1,6 @@
 #include "energies.h"
- 
+
+//to compute total kinetic energy
 double T(size_t Natoms, double** velocity, double* mass) {
     double kinetic_energy = 0.0;
     for (size_t i = 0; i < Natoms; i++) {
@@ -9,3 +10,9 @@ double T(size_t Natoms, double** velocity, double* mass) {
         kinetic_energy += 0.5 * mass[i] * v2;
     }
     return kinetic_energy;
+}
+
+//to compute total energy
+double E(double T, double V) {
+    return T + V;
+}
