a53cd19f4c9b57c916a2d98d3979d74e44a4b198
BesaMulajKrasniqi <besasmulaj@gmail.com>
2025-01-05 15:25:13 +0100

    Create distance.h

---
+++ project3/src/distance.h
@@ -0,0 +1,10 @@
+#ifndef DISTANCE_H
+#define DISTANCE_H
+ 
+#include <math.h>
+#include <stdlib.h>
+ 
+// Function to compute distances between atoms
+void compute_distances(size_t Natoms, double** coord, double** distance);
+ 
+#endif // DISTANCE_H
