b5154569633cbf2fd67f3480719fcca1ebfa0127
BesaMulajKrasniqi <besasmulaj@gmail.com>
2025-01-05 15:26:32 +0100

    Update energies.c

--- project3/src/energies.c
+++ project3/src/energies.c
@@ -1,1 +1,11 @@
-
+#include "energy.h"
+ 
+double T(size_t Natoms, double** velocity, double* mass) {
+    double kinetic_energy = 0.0;
+    for (size_t i = 0; i < Natoms; i++) {
+        double v2 = velocity[i][0] * velocity[i][0] +
+                    velocity[i][1] * velocity[i][1] +
+                    velocity[i][2] * velocity[i][2];
+        kinetic_energy += 0.5 * mass[i] * v2;
+    }
+    return kinetic_energy;
