86894cc07113c3e27712a7dfb25aa11fa09dbb74
BesaMulajKrasniqi <besasmulaj@gmail.com>
2025-01-05 15:27:40 +0100

    Create energies.h

---
+++ project3/src/energies.h
@@ -0,0 +1,9 @@
+#ifndef ENERGIES_H
+#define ENERGIES_H
+ 
+#include <stdlib.h>
+ 
+// Function to compute kinetic energy
+double T(size_t Natoms, double** velocity, double* mass);
+ 
+#endif // ENERGIES_H
