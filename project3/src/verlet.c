03ef45f8faf0a2a6675a4c191dbe1574b6c6d4a4
BesaMulajKrasniqi <besasmulaj@gmail.com>
2025-01-05 16:04:12 +0100

    Update verlet.c

--- project3/src/verlet.c
+++ project3/src/verlet.c
@@ -1,1 +1,17 @@
-
+#include "verlet.h"
+ 
+void verlet_step(size_t Natoms, double** coord, double** velocity, double** acceleration, double dt) {
+    for (size_t i = 0; i < Natoms; i++) {
+        for (size_t k = 0; k < 3; k++) {
+            coord[i][k] += velocity[i][k] * dt + 0.5 * acceleration[i][k] * dt * dt;
+            velocity[i][k] += 0.5 * acceleration[i][k] * dt;
+        }
+    }
+    // Recalculate accelerations
+    // Call compute_acc here
+    for (size_t i = 0; i < Natoms; i++) {
+        for (size_t k = 0; k < 3; k++) {
+            velocity[i][k] += 0.5 * acceleration[i][k] * dt;
+        }
+    }
+}
