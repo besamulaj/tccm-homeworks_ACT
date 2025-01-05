a3e0f8c9e95dd362f6bc8337a08910f932047ce4
BesaMulajKrasniqi <besasmulaj@gmail.com>
2025-01-05 15:38:42 +0100

    Update lj_potential.c

--- project3/src/lj_potential.c
+++ project3/src/lj_potential.c
@@ -1,1 +1,17 @@
-
+#include <math.h>
+#include "lj_potential.h"
+ 
+double V(double EPSILON, double SIGMA, size_t Natoms, double** distance) {
+    double potential_energy = 0.0;
+    for (size_t i = 0; i < Natoms; i++) {
+        for (size_t j = i + 1; j < Natoms; j++) {
+            double r = distance[i][j];
+            if (r > 0) {
+                double r6 = pow(SIGMA / r, 6);
+                double r12 = r6 * r6;
+                potential_energy += 4 * EPSILON * (r12 - r6);
+            }
+        }
+    }
+    return potential_energy;
+}
