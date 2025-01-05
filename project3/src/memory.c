3795f7ec84f43a1c2e576db2fa6a496438767a74
bilgecetinn <bilgecetin306@gmail.com>
2025-01-05 04:33:20 +0100

    Update memory.c

--- project3/src/memory.c
+++ project3/src/memory.c
@@ -3,29 +3,29 @@
 
 // Allocate a 2D array
 double** malloc_2d(size_t m, size_t n) {
-    double** array = malloc(m * sizeof(double*)); 
-    if (!array) {
+    double** a = malloc(m * sizeof(double*)); 
+    if (a==NULL) {
         fprintf(stderr, "Error: Unable to allocate memory for 2D array pointers.\n");
         return NULL;
     }
-
-    array[0] = malloc(m * n * sizeof(double));
-    if (!array[0]) {
+//Allocate a contiguous block of memory for the elements of 2D array
+    a[0] = malloc(m * n * sizeof(double));
+    if (a[0] ==NULL) {
         fprintf(stderr, "Error: Unable to allocate memory for 2D array data.\n");
-        free(array);
+        free(a);
         return NULL;
     }
 
     //pointers to each row
     for (size_t i = 1; i < m; i++) {
-        array[i] = array[i - 1] + n;
+        a[i] = a[i - 1] + n;
     }
-    return array;
+    return a;
 }
-
-void free_2d(double** array) {
-    if (array) {
-        free(array[0]); 
-        free(array);   
-    }
+//Function to free memory for a 2D array
+void free_2d(double** a) {
+    free(a[0]); //free the contiguous block of memory
+    a[0]= NULL
+    free(a);   //free the array of pointers 
+    
 }
