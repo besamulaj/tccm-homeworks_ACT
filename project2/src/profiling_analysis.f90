2990ad68bbddc97d4908cd234c890abab81c7693
BesaMulajKrasniqi <besasmulaj@gmail.com>
2025-01-10 00:53:57 +0100

    Update profiling_analysis.f90

--- project2/src/profiling_analysis.f90
+++ project2/src/profiling_analysis.f90
@@ -1,1 +1,24 @@
-
+module profiling_analysis
+  implicit none
+  contains
+ 
+  subroutine profile_methods(sparse_time, dense_time, blas_time, matrix_size)
+    real(8), intent(in) :: sparse_time, dense_time, blas_time
+    integer, intent(in) :: matrix_size
+    real(8) :: efficiency_sparse, efficiency_dense, efficiency_blas
+ 
+    ! Compute efficiencies relative to size
+    efficiency_sparse = matrix_size / sparse_time
+    efficiency_dense = matrix_size / dense_time
+    efficiency_blas = matrix_size / blas_time
+ 
+    print *, "Profiling Results:"
+    print *, "----------------------------------------"
+    print *, "Matrix Size:", matrix_size
+    print *, "Sparse Efficiency:", efficiency_sparse, " elements/sec"
+    print *, "Dense Efficiency:", efficiency_dense, " elements/sec"
+    print *, "BLAS Efficiency:", efficiency_blas, " elements/sec"
+    print *, "----------------------------------------"
+ 
+  end subroutine profile_methods
+end module profiling_analysis
