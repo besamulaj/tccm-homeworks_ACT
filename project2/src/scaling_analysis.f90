c56d428760194a3a9afe66f4a9b9b481f1620b1e
BesaMulajKrasniqi <besasmulaj@gmail.com>
2025-01-10 00:52:13 +0100

    Update scaling_analysis.f90

--- project2/src/scaling_analysis.f90
+++ project2/src/scaling_analysis.f90
@@ -1,1 +1,19 @@
-
+module scaling_analysis
+  implicit none
+  contains
+ 
+  subroutine analyze_scaling(matrix_sizes, filling_degrees, sparse_times, dense_times, blas_times)
+    integer, intent(in) :: matrix_sizes(:)
+    real(8), intent(in) :: filling_degrees(:), sparse_times(:), dense_times(:), blas_times(:)
+    integer :: i
+ 
+    print *, "Scaling Analysis Results:"
+    print *, "----------------------------------------"
+    print *, "Size    Filling    Sparse Time    Dense Time    BLAS Time"
+    do i = 1, size(matrix_sizes)
+      print *, matrix_sizes(i), filling_degrees(i), sparse_times(i), dense_times(i), blas_times(i)
+    end do
+    print *, "----------------------------------------"
+ 
+  end subroutine analyze_scaling
+end module scaling_analysis
