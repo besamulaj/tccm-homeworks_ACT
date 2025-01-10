adb8df37fa1de2587f9d8876f55e6e589d33a38b
BesaMulajKrasniqi <besasmulaj@gmail.com>
2025-01-10 15:43:41 +0100

    Update multiply_dense_matrix.f90

--- project2/src/multiply_dense_matrix.f90
+++ project2/src/multiply_dense_matrix.f90
@@ -1,1 +1,18 @@
-
+module multiply_dense_matrix
+    implicit none
+contains
+    subroutine dense_multiply(A, B, Result)
+        real(8), intent(in) :: A(:,:), B(:,:)
+        real(8), intent(out) :: Result(size(A,1), size(B,2))
+        integer :: i, j, k
+ 
+        Result = 0.0d0
+        do i = 1, size(A,1)
+            do j = 1, size(B,2)
+                do k = 1, size(A,2)
+                    Result(i,j) = Result(i,j) + A(i,k) * B(k,j)
+                end do
+            end do
+        end do
+    end subroutine dense_multiply
+end module multiply_dense_matrix
